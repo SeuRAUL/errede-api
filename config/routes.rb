93712bb5a1e92123b28517ea44223079493d3110
seuraul <seuraul.0@gmail.com>
2023-01-15 17:01:38 -0300

    LaunchesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "launches#launches"
+  root 'launches#launches'
+
+  get '/next', to: 'launches#next'
+  get '/latest', to: 'launches#latest'
+  get '/past', to: 'launches#past'
+  get '/upcoming', to: 'launches#upcoming'
 end
